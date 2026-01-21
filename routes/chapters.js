4d2d25353a1f9bd3f640e192aa3a40b37afea6fb
Dexter Tan <ashuragi007@gmail.com>
2026-01-21 20:59:29 +0100

    chapter API

---
+++ routes/chapters.js
@@ -0,0 +1,12 @@
+import express from "express";
+import validateChapterLength from "../modules/validateChapterLength.mjs";
+
+const chapterRouter= express.Router();
+
+
+chapterRouter.post("/", validateChapterLength, (req, res) => {
+    res.json({
+        message: "Chapter accepted",
+        content: req.body.content,
+    });
+});
